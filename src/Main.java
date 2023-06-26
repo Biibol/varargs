b2225368ad7eb02de1b3963eb1b7cc5203926636
biibo <biibolot23@gmail.com>
2023-06-26 21:42:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        method(
+//                "BIIBOLOT", "ERBOL", "BEKA",
+//                "SIMA", "NURIK", "RUSLAN","ILIAZ","ASAN","NARYNBEK");
+//    }
+//    static void method(String[]Array) {
+//        for (int i;Array){
+//            System.out.println("Мен сени жакшы кором"+varargs);
+//        }
+//    }
+
+//
+//        method(12, 23, 3, 4, 35, 4, 5, 56, 5, 67, 7, 8, 79, 89);
+//    }
+//
+//
+//    static void method(int... varargs) {
+//        int s = 0;
+//        int b = 0;
+//        for (int i = 0; i < varargs.length; i++) {
+//            s += varargs[i];
+//            b++;
+//            System.out.println(s);
+////        }
+//        System.out.println("кол" + b);
+//    }
+//
+//        String[] s = new String[]{"BIIBOLOT", "ERBOL", "BEKA",
+//                "SIMA", "NURIK", "RUSLAN", "ILIAZ", "ASAN", "NARYNBEK", "TIMUR"};
+//        for (String i : s) {
+//            switch (i){
+//                case "TIMUR":
+//                    System.out.println("Агай  "+i);
+//
+//            }
+//                System.out.println("Мен сени жакшы кором " + i);
+//
+//
+
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+    static void Method(String b) {
+
+        System.out.println(b.toUpperCase());
+        System.out.println(b.trim());
+        System.out.println(b.);
+        System.out.println();
+        System.out.println();
+    }
+}
+
+
+
